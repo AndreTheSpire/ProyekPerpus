588df380c2ebe8e96395ea5333d5ff366b6af164
Joshua Mishael <64304163+warjoshuamw@users.noreply.github.com>
2021-06-04 12:25:51 +0700

    QOL changes

--- PerpusPCS/PerpusPCS/ConnectionPage.xaml.cs
+++ PerpusPCS/PerpusPCS/ConnectionPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using Oracle.DataAccess.Client;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,7 +14,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-using Oracle.DataAccess.Client;
+using System.IO;
 
 namespace PerpusPCS
 {
@@ -23,11 +26,70 @@ namespace PerpusPCS
         public static OracleConnection conn;
         public static String source, userId, pass;
 
-        
+        string path = Directory.GetCurrentDirectory() + "\\logins.txt";
         public ConnectionPage()
         {
-            InitializeComponent();
+             InitializeComponent();
             WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            loadSavedLogins();
+        }
+
+        private void dgvLogins_Loaded(object sender, RoutedEventArgs e)
+        {
+            //dgvLogins.Columns[1].Header = "Database";
+            //dgvLogins.Columns[2].Header = "Username";
+            //dgvLogins.Columns[3].Header = "Password";
+        }
+
+        private void btnApply_Click(object sender, RoutedEventArgs e)
+        {
+            if (listLogins.SelectedIndex != -1)
+            {
+                string line = listLogins.SelectedItem.ToString();
+                Console.WriteLine(line);
+                string[] lines = line.Split(' ');
+                TxtDatabase.Text = lines[0];
+                TxtPass.Text = lines[1];
+                TxtUsername.Text = lines[2];
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string line = TxtDatabase.Text + ' ' + TxtUsername.Text+ ' ' +  TxtPass.Text;
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(line);
+                sw.Close();
+            }
+            loadSavedLogins();
+        }
+
+        public void loadSavedLogins()
+        {
+            listLogins.Items.Clear();
+            if (File.Exists(path))
+            {
+                Console.WriteLine("file ada pada : " + path);
+                StreamReader sr = new StreamReader(path);
+                string line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    listLogins.Items.Add(line);
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            else
+            {
+                Console.WriteLine("file tidak ada pada : "+path);
+                using (FileStream fs = File.Create(path))
+                {
+                    
+                }
+            }
         }
         private void btnConnect_Click(object sender, RoutedEventArgs e)
         {
